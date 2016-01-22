ea0806b943d6831eca140cacabd854c1a43ffaf3
denis <mail_box2@mail.ru>
2016-01-22 20:07:09 +0300

    beta

--- odata1c.js
+++ odata1c.js
@@ -12,14 +12,18 @@ function Odata1c(url, login, pass) {
 		request.get({ url: this.options.url + '$metadata', header: this.options.headers }, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				callBackFunc(JSON.parse(body));
+			}else{
+				callBackFunc(error);
 			}
 		})
 			.auth(this.login, this.pass, false)
 	}
 	this.getCatalog = function (object1c, callBackFunc) {
 		request.get({ url: this.options.url +'/Catalog_'+encodeURI(object1c), headers: this.options.headers }, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-					callBackFunc(JSON.parse(body));
+			if (!error && response.statusCode == 200) {					
+                       callBackFunc(JSON.parse(body));
+			}else{
+				callBackFunc(error);
 			}
 		})
 			.auth(this.login, this.pass, false)
@@ -27,7 +31,9 @@ function Odata1c(url, login, pass) {
 	this.getDocument = function (object1c, callBackFunc) {
 		request.get({ url: this.options.url +'/Document_'+encodeURI(object1c), headers: this.options.headers }, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
-					callBackFunc(JSON.parse(body));
+				callBackFunc(JSON.parse(body));
+			}else{
+				callBackFunc(error);
 			}
 		})
 			.auth(this.login, this.pass, false)
@@ -35,16 +41,44 @@ function Odata1c(url, login, pass) {
 	this.getConstant= function (object1c, callBackFunc) {
 		request.get({ url: this.options.url +'/Constant_'+encodeURI(object1c), headers: this.options.headers }, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
-					callBackFunc(JSON.parse(body));
+				callBackFunc(JSON.parse(body));
+			}else{
+				callBackFunc(error);
+			}
+		})
+			.auth(this.login, this.pass, false)
+	}	
+    this.getInformationRegisters= function (object1c, callBackFunc) {
+		request.get({ url: this.options.url +'/InformationRegister_'+encodeURI(object1c), headers: this.options.headers }, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				callBackFunc(JSON.parse(body));
+			}else{
+				callBackFunc(error);
 			}
 		})
 			.auth(this.login, this.pass, false)
 	}	
 
-	this.post = function () { }
-	this.delete = function () { }
-	this.patch = function () { }
+    
 }
 
-var odata = new Odata1c('http://hostname', 'login', 'password');
-odata.getCatalog("ВнешниеОбработки",function(result){console.log(result)});
+getPaySlip = function(id,date,url,login,pass){
+    this.date=date;
+    this.odata = new Odata1c(url, login, pass);
+    this.odata.getCatalog("СотрудникиОрганизаций?$filter=Code eq '"+id+"'", function(result){this.getData(result.value[0].Физлицо_Key)});
+    this.getData=function(guid){
+      this.odata.getDocument("ФормированиеДанныхДляВебСервиса1СБитрикс?$filter=ПериодРегистрации eq datetime'"+this.date+"'",
+        function(result){
+                for(var i in result.value[0].ФизическиеЛица){
+                    if(result.value[0].ФизическиеЛица[i].ФизЛицо_Key==guid){
+                        console.log(new Buffer(result.value[0].ФизическиеЛица[i].РасчетныеЛистки_Base64Data,'base64').toString());
+                    }
+                    
+                }    
+      });
+}
+}
+//getPaySlip('0000000000','2015-12-01T00:00:00','','','');
+exports.getPaySlip = getPaySlip;
+
+
